1b35279fd082d5a525b150dc09a643085bb4ab93
Stevie <121079784+StevieSAT@users.noreply.github.com>
2023-03-09 18:17:27 -0500

    Update loop_compre2.py

--- loop_compre2.py
+++ loop_compre2.py
@@ -1,10 +1,10 @@
-```
-Here’s a harder question which brings a few things together.
+# Question
+# Here’s a harder question which brings a few things together.
+# Many organizations have user ids which are constrained in some way. 
+# Imagine you work at an internet service provider and the user ids are all two letters followed by two numbers (e.g. aa49). 
+# Your task at such an organization might be to hold a record on the billing activity for each possible user. 
+# Write an initialization line as a single list comprehension which creates a list of all possible user ids. Assume the letters are all lower case.
 
-Many organizations have user ids which are constrained in some way. Imagine you work at an internet service provider and the user ids are all two letters followed by two numbers (e.g. aa49). Your task at such an organization might be to hold a record on the billing activity for each possible user. 
-
-Write an initialization line as a single list comprehension which creates a list of all possible user ids. Assume the letters are all lower case.
-```
 
 lowercase = 'abcdefghijklmnopqrstuvwxyz'
 digits = '0123456789'
